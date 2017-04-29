275d27a0e6355061de721dc8c2246acc48f28b0d
Alejandro Mesa Suarez <alejandro.mesa1991@gmail.com>
2017-04-29 18:28:15 -0400

    Smooth Scrolling and Fixes

--- src/Footer.js
+++ src/Footer.js
@@ -3,20 +3,22 @@ import './Footer.css';
 
 class Footer extends Component {
 
+
+
     render() {
         return (
-        <div className="footerSection">
+        <div href="#section1" className="footerSection">
 
         <div className="social">
         
-        <a href="https://github.com/alemesa1991"><i className="fa fa-2x fa-github" aria-hidden="true"></i></a>   
-        <a href="https://twitter.com/alelikesramen"><i className="fa fa-2x fa-twitter" aria-hidden="true"></i></a> 
-        <a href="https://www.linkedin.com/in/alejandro-mesa-suarez-604916a2/"><i className="fa fa-2x fa-linkedin" aria-hidden="true"></i></a>
-        <a href="https://github.com/alemesa1991/React-News-App">Repo <i className="fa fa-code-fork" aria-hidden="true"></i> </a>
+        <a href="https://github.com/alemesa1991" target="_blank"><i className="fa fa-2x fa-github" aria-hidden="true"></i></a>   
+        <a href="https://twitter.com/alelikesramen" target="_blank"><i className="fa fa-2x fa-twitter" aria-hidden="true"></i></a> 
+        <a href="https://www.linkedin.com/in/alejandro-mesa-suarez-604916a2/" target="_blank"><i className="fa fa-2x fa-linkedin" aria-hidden="true"></i></a>
+        <a id="repo" href="https://github.com/alemesa1991/React-News-App" target="_blank">Repo <i className="fa fa-code-fork" aria-hidden="true"></i> </a>
         
         </div>
-        
-        <a href="#"><i className="fa fa-2x fa-arrow-up" aria-hidden="true"></i></a>
+      
+        <a className="smoothScroll" href="#"><i className="fa fa-2x fa-arrow-up" aria-hidden="true"></i></a>
             
         <span className="coffee">
         Made with <i className="fa fa-coffee" aria-hidden="true"></i>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,92 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-
+/*DOM Render*/
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+/*Smooth Scrolling Snippet - NO DEPENDENCIES*/
+//Add the class smoothScroll to the links where you want to apply smoothScroll
+// or it will messed up all the links
+var links = document.querySelectorAll(".smoothScroll");
+const transitionSpeed = 10; //transition intervals (ms)
+const initialSpeed = 20; //initial speed (px)
+const acceleration = 0.15; //transition acceleration (px)
+const offset = 10; //offset so the element is not totally in the border (px)
+//const = HTMLMenuElement.height;
+
+links.forEach(link => link.addEventListener("click", function(e) {
+
+    //Prevent Default Behaviour
+    e.preventDefault();
+    e.stopPropagation();
+
+    //declare the objective element
+    var goingTo;
+    var hrefID = link.getAttribute('href');
+
+    //In case the # is entered
+    if (hrefID === '#') {
+        goingTo = document.body;
+    } else {
+        goingTo = document.querySelector(hrefID);
+    }
+
+
+
+    // var distance = goingTo.offsetTop - link.offsetTop;
+
+    var totalTraveled = 0;
+
+
+    if (link.offsetTop > goingTo.offsetTop) {
+        //IF GOING UP
+        let speed = -initialSpeed;
+        let distance = goingTo.offsetTop - window.scrollY + offset;
+        console.log("Going up by " + distance + "px");
+
+        //Start intervals movement
+        let interval = setInterval(function() {
+            window.scrollBy(0, speed);
+            totalTraveled += speed;
+            speed = Math.round(speed - acceleration);
+
+            //In case it goes over it will 
+            //make a small jump to fix it
+            if (totalTraveled <= distance) {
+                window.scrollBy(0, (totalTraveled - distance));
+                console.log("Fixing distance by " + (totalTraveled - distance));
+                clearInterval(interval);
+            }
+
+
+        }, transitionSpeed);
+
+
+    } else {
+        //IF GOING DOWN
+        let distance = goingTo.offsetTop - window.scrollY - offset;
+        let speed = initialSpeed;
+        console.log("Going down by " + distance + "px");
+        
+        //Start intervals movement
+        let interval = setInterval(function() {
+            window.scrollBy(0, speed);
+            totalTraveled += speed;
+            speed = Math.round(speed + acceleration);
+
+            //In case it goes over it will 
+            //make a small jump to fix it
+            if (totalTraveled >= distance) {
+                window.scrollBy(0, -(totalTraveled - distance));
+                console.log("Fixing distance by " + (-(totalTraveled - distance)));
+                clearInterval(interval);
+            }
+        }, transitionSpeed);
+
+    }
+
+}));
