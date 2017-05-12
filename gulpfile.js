7d35d1585e618783283e10ee3ce53ab73708e66d
Alejandro Mesa Suarez <alejandro.mesa1991@gmail.com>
2017-05-12 02:52:41 -0400

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var surge = require('gulp-surge');
 
 gulp.task('deploy', [], function () {
   return surge({
-    project: './build',         // Path to your static build directory
+    project: './build/',         // Path to your static build directory
     domain: 'news-app.surge.sh'  // Your domain or Surge subdomain
   });
 });
